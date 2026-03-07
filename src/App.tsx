cdeb9c90c18683913eed07c28bf2553d3dce1fc3
Ihor Ishchenko <torgen1997@gmail.com>
2026-03-07 23:30:35 +0200

    SCRUM-1: []Test 1asf askjf

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="rounded-lg bg-slate-100 p-6 shadow-sm">
       <h1 className="text-2xl font-semibold text-slate-800">Hello, React!</h1>
-      <p className="text-slate-600">Basic React app with Vite.</p>
+      <p className="text-slate-600">This is testing automation</p>
     </div>
   )
 }
